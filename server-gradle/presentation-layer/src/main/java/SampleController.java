0c5fce5c8d4bed8029c5496bc9cbe38c5805788f
Ovidiu CONEAC <ovidiu.coneac@gmail.com>
2017-02-11 23:54:44 +0200

    Commit maven and gradle version

--- server-gradle/presentation-layer/src/main/java/SampleController.java
+++ server-gradle/presentation-layer/src/main/java/SampleController.java
@@ -1,3 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Ovidiu CONEAC on 2/11/2017.
  */
--- server/presentation-layer/src/main/java/ro/ovidiuconeac/backend/presentationlayer/Application.java
+++ server/presentation-layer/src/main/java/ro/ovidiuconeac/backend/presentationlayer/Application.java
@@ -3,5 +3,10 @@ package ro.ovidiuconeac.backend.presentationlayer;
 /**
  * Created by Ovidiu CONEAC on 2/11/2017.
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
 }
