bbdb05c732ac561af8d19de8f5047cf3fc093c0b
Ovidiu CONEAC <ovidiu.coneac@gmail.com>
2017-02-11 23:53:15 +0200

    Initial commit for maven project

---
+++ server-gradle/presentation-layer/src/main/java/SampleController.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Ovidiu CONEAC on 2/11/2017.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
---
+++ server/presentation-layer/src/main/java/ro/ovidiuconeac/backend/presentationlayer/Application.java
@@ -0,0 +1,7 @@
+package ro.ovidiuconeac.backend.presentationlayer;
+
+/**
+ * Created by Ovidiu CONEAC on 2/11/2017.
+ */
+public class Application {
+}
